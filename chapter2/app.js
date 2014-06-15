aa34e1252f6dc2a1bf3ab12cc682ccf3da194f46
Jason Krol <jasonk@aweber.com>
2014-06-14 20:53:47 -0400

    Update style for chapter 2 code

--- chapter2/app.js
+++ chapter2/app.js
@@ -1,15 +1,15 @@
 var MongoClient = require('mongodb').MongoClient;
- 
+
 var dbhost = 'mongodb://localhost:27017/test',
     myCollection = 'chapter2';
- 
+
 var seedData = function(db, callback) {
     db.collection(myCollection).find({}, {}, {})
         .toArray(
             function(err, docs) {
                 if (docs.length <= 0) {
                     console.log('No data. Seeding...');
- 
+
                     var ihandler = function(err, recs) {
                         if (err) {
                             console.log(err);
@@ -17,10 +17,10 @@ var seedData = function(db, callback) {
                         }
                         inserted++;
                     }
- 
+
                     var toinsert = 5,
                         inserted = 0;
- 
+
                     db.collection(myCollection).insert({
                         'Title': 'Snow Crash',
                         'Author': 'Neal Stephenson'
@@ -41,9 +41,9 @@ var seedData = function(db, callback) {
                         'Title': 'CyberStorm',
                         'Author': 'Matthew Mather'
                     }, ihandler);
- 
-                    var sync = setInterval(function(){
-                        if(inserted === toinsert) {
+
+                    var sync = setInterval(function() {
+                        if (inserted === toinsert) {
                             clearInterval(sync);
                             callback(db);
                         }
@@ -53,30 +53,32 @@ var seedData = function(db, callback) {
                 callback(db);
                 return;
             }
-        );
+    );
 }
- 
+
 var showDocs = function(db) {
     console.log("Listing books:");
     var options = {
-        sort: [['Title',1]]
+        sort: [
+            ['Title', 1]
+        ]
     };
     db.collection(myCollection).find({}, {}, options)
         .toArray(
             function(err, docs) {
                 if (err) throw err;
- 
-                for(var d = 0; d < docs.length; d++) {
+
+                for (var d = 0; d < docs.length; d++) {
                     console.log(docs[d].Title + '; ' + docs[d].Author);
                 }
- 
+
                 db.close();
             }
     );
 }
- 
-MongoClient.connect(dbhost, function(err, db){
+
+MongoClient.connect(dbhost, function(err, db) {
     if (err) throw err;
- 
+
     seedData(db, showDocs);
 });
